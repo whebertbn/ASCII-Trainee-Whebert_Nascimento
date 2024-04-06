1a915909e6f2caf02ef6c4570fa528df02c85316
Whebert <67442412+whebertbn@users.noreply.github.com>
2024-04-06 17:59:26 -0300

    Add files via upload

---
+++ modulo_d/ex015/script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'bebe-m.jpg')
+            } else if(idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else if(idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adulto-m.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idoso-m.jpg')   
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'bebe-f.jpg')
+            } else if(idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'jovem-f.jpg')
+            } else if(idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adulto-f.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idoso-f.jpg')   
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+
+    }
+}
